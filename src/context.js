9bb60f311fe6c96d52b73fded8fee69c78507faf
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-05-01 05:35:15 +0000

    Fixed no access control

--- src/context.js
+++ src/context.js
@@ -10,7 +10,7 @@ export class Provider extends Component {
     }
 
     componentDidMount(){
-        axios.get(`https://api.musixmatch.com/ws/1.1/chart_name=top&page=1&page_size=10&country=IN&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=in&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY }`)
         .then(res => console.log(res.data))
         .catch(err => console.log(err))
     }
